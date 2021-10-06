59b02374046ae34d3530557c6d7018731a0a2c36
Libin Luvis <libinluvis10@gmail.com>
2021-10-06 17:53:37 +0530

    the git push

--- App.js
+++ App.js
@@ -1,11 +1,75 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import * as BackgroundFetch from "expo-background-fetch";
+import * as TaskManager from "expo-task-manager"
+import axios from 'axios';
 export default function App() {
+
+  const [value,setValue] = useState()
+
+  useEffect(()=>{
+    RegisterBackgroundTask();
+  },[])
+
+  const handleAddTask = () => {
+    const config = {
+      headers: {
+        "Accept":"application/json",
+        "Content-Type": "application/json",
+      },
+    };
+    const body = JSON.stringify({ title : value });
+
+    const api="api";
+    axios.post(api,body,config).then((res)=>{
+    console.log("Data is ",res.data)
+    
+    }).catch((err)=>{
+      console.log(err,"error");
+    })
+  }
+
+  const TASK_NAME = "BACKGROUND_TASK"
+  TaskManager.defineTask(TASK_NAME, () => {
+    try {
+      // fetch data here...
+      const receivedNewData = new Date().toString(); 
+      console.log( receivedNewData)
+      setValue(receivedNewData);
+      return receivedNewData
+        ? BackgroundFetch.Result.NewData
+        : BackgroundFetch.Result.NoData
+    } catch (err) {
+      return BackgroundFetch.Result.Failed
+    }
+  })
+   const RegisterBackgroundTask = async () => {
+    try {
+      await BackgroundFetch.registerTaskAsync(TASK_NAME, {
+        minimumInterval: 5,
+        stopOnTerminate: false,
+        startOnBoot: true,
+       
+      })
+      console.log("Task registered")
+    } catch (err) {
+      console.log("Task Register failed:", err)
+    }
+  }
+  
+  if(value){
+    handleAddTask();
+    console.log('function called')
+   }
+   else{
+     console.log("function not calling")
+   }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>hello world!</Text>
       <StatusBar style="auto" />
     </View>
   );
